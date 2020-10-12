1c6013e060ca67f40239f0104fc370de63b4b585
Sayed Sadat <sysadat@ucdavis.edu>
2020-10-11 17:55:14 -0700

    Finished peekAheadChar()

--- PCSClass.java
+++ PCSClass.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 public class PCSClass implements PeekableCharacterStream {
     // Class attributes
     int currentIndex;
+    int contentOfFileLength;
     String contentOfFile;
 
     // Constructor
@@ -18,10 +19,12 @@ public class PCSClass implements PeekableCharacterStream {
 
         String fileAsString = Files.readString(fileName);
 
+
         // System.out.println("Printing now ...");
         // System.out.println(fileAsString);
         
         contentOfFile = fileAsString;
+        contentOfFileLength = contentOfFile.length();
         currentIndex = 0;
 
         // System.out.println("Content of file is: " + contentOfFile);
@@ -32,10 +35,9 @@ public class PCSClass implements PeekableCharacterStream {
 
     // Returns true if more characters are available, false otherwise
     public boolean moreAvailable() {
-        int contentOfFileLength = contentOfFile.length();
-        System.out.println("Length of string is: " + contentOfFileLength);
+        // System.out.println("Length of string is: " + contentOfFileLength);
 
-        if (currentIndex >= (contentOfFileLength)) {
+        if (currentIndex >= contentOfFileLength) {
             return false;
         } else {
             return true;
@@ -46,35 +48,60 @@ public class PCSClass implements PeekableCharacterStream {
     // the character. If no more characters are available -1 is returned.
     public int peekNextChar() {
         boolean isAvaliable = moreAvailable();
-        System.out.println("In peekNextChar, result is: " + isAvaliable);
+        // System.out.println("In peekNextChar, result is: " + isAvaliable);
         char currentCharacter;
         int characterAsInt = 0;
         if (isAvaliable) {
             currentCharacter = contentOfFile.charAt(currentIndex);
-            System.out.println("Current character is: " + currentCharacter);
-            System.out.println("characterAsInt before is: " + characterAsInt);
+            // System.out.println("Current character is: " + currentCharacter);
+            // System.out.println("characterAsInt before is: " + characterAsInt);
             characterAsInt = currentCharacter;
-            System.out.println("characterAsInt is: " + characterAsInt);
+            // System.out.println("characterAsInt is: " + characterAsInt);
+            return characterAsInt;
+        } else {
+            return -1;
+        }
+    }
+    // Returns the character ahead in the stream without consuming the
+    // the character. peekAheadChar(0) returns the same character as
+    // peekNextChar(). If no more characters are available at that position
+    // -1 is returned.
+    public int peekAheadChar(int ahead) {
+        // The value for ahead is the value of the current index added to the value in ahead
+        int aheadIndex = ahead + currentIndex;
+        char currentCharacter;
+        int characterAsInt = 0;
+        System.out.println("Ahead index value is: " + aheadIndex);
+        System.out.println("Ahead value is: " + ahead + "and aheadIndex is: " + currentIndex);
+        if (aheadIndex >= contentOfFileLength) {
+            return -1;
+        } else {
+            currentCharacter = contentOfFile.charAt(aheadIndex);
+            characterAsInt = currentCharacter;
+            return characterAsInt;
+        }
+    }
+
+
+    // Returns the next character and consumes it. If no more characters are
+    // available -1 is returned.
+    public int getNextChar() {
+        boolean isAvaliable = moreAvailable();
+        // System.out.println("In peekNextChar, result is: " + isAvaliable);
+        char currentCharacter;
+        int characterAsInt = 0;
+        if (isAvaliable) {
+            currentCharacter = contentOfFile.charAt(currentIndex);
+            // System.out.println("Current character is: " + currentCharacter);
+            // System.out.println("characterAsInt before is: " + characterAsInt);
+            characterAsInt = currentCharacter;
+            // System.out.println("characterAsInt is: " + characterAsInt);
             return characterAsInt;
         } else {
             return -1;
         }
     }
 /* 
-    // Returns the character ahead in the stream without consuming the
-    // the character. peekAheadChar(0) returns the same character as
-    // peekNextChar(). If no more characters are available at that position
-    // -1 is returned.
-    public int peekAheadChar(int ahead) {
-        
-    }
-
-    // Returns the next character and consumes it. If no more characters are
-    // available -1 is returned.
-    public int getNextChar() {
-        
-    }
-
      // Closes the stream.
      public void close() {
         
@@ -95,8 +122,9 @@ public class PCSClass implements PeekableCharacterStream {
         PCSClass myObj = new PCSClass(inputtedFileName);
         boolean result = myObj.moreAvailable();
         int peekResult = myObj.peekNextChar();
-
-        System.out.println("Peek result is: " + peekResult);
+        int peekAheadVal = 1;
+        // System.out.println("Peek Ahead result is: " + myObj.peekAheadChar(peekAheadVal));
+        // System.out.println("Peek result is: " + peekResult);
 
         // System.out.println("Result is: " + result);
         // System.out.println("Content of file is: " + myObj.contentOfFile);
--- PeekableCharacterStream.java
+++ PeekableCharacterStream.java
@@ -10,7 +10,7 @@
 public interface PeekableCharacterStream{
     public boolean moreAvailable();
     public int peekNextChar();
-    // public int peekAheadChar(int ahead);
-    // public int getNextChar();
+    public int peekAheadChar(int ahead);
+    public int getNextChar();
     // public void close();
 }
